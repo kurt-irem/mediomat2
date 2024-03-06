9c34469efb7674b131124a47f84bbfe78a6b40e4
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2024-03-06 14:31:49 +0100

    build for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
-module.exports = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
-}
+
+};
+
 export default nextConfig;
