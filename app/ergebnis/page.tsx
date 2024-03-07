8e9a4e1d2c0b4ddbed579cedef6313cc4450e83d
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2024-03-07 16:06:53 +0100

    repeat button

--- app/ergebnis/page.tsx
+++ app/ergebnis/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import VorschlagCard from "@/components/cards/vorschlagCard";
 import test from "../../data/media.json";
-import {useSearchParams} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 //import {useEffect, useState} from 'react';
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
+import textData from "@/data/texte.json";
+import useAnimationToggle from "@/hooks/useAnimationToggle";
 
 interface Bewertung {
     wert: number;
@@ -17,9 +19,11 @@ interface FragenKarteProps {
 
 
 const Ergebnis = () => {
-    let mediaResults: { [index: string]: number } = {}
-
+    const router = useRouter();
+    const animate = useAnimationToggle(7000);
     const searchParams = useSearchParams()
+
+    let mediaResults: { [index: string]: number } = {}
     let answers = JSON.parse(searchParams.get("answer") as string)
 
     for (let medium of test) {
@@ -58,18 +62,27 @@ const Ergebnis = () => {
             </h2>
 
             <Suspense fallback={<div>Loading...</div>}>
-            <div className="flex justify-center py-2 card">
-                {favoriteCards.map((card, index) => (
+                <div className="flex justify-center py-2 card">
+                    {favoriteCards.map((card, index) => (
                         <VorschlagCard
-                        key={index}
-                        name={card.name}
-                        beschreibung={card.beschreibung}
-                        image={card.image}
-                        mediumArt={card.mediumArt}
-                        url={card.url}/>
-                ))}
-            </div>
+                            key={index}
+                            name={card.name}
+                            beschreibung={card.beschreibung}
+                            image={card.image}
+                            mediumArt={card.mediumArt}
+                            url={card.url}/>
+                    ))}
+                </div>
             </Suspense>
+            <button
+                onClick={() => router.push("/befragung")}
+                className={`${
+                    animate ? "animate__animated animate__headShake" : ""
+                } mt-28 px-6 py-4 bg-blue text-white font-light text-2xl rounded-lg  shadow-md flex flex-row-reverse gap-3 justify-center items-center w-[400px] transition hover:shadow-2xl hover:scale-105 ease-in`}
+            >
+                <i className="pi pi-arrow-right" style={{fontSize: "1rem"}}/>
+                {textData.repeat}
+            </button>
         </div>
     );
 }
