85ea53debfa60fb0636a44efc91d0c2f4330631a
cedric.hohensee <cedricc.business@gmail.com>
2024-03-28 10:01:57 +0100

    change style && add new button && add text for medienuebersichtBtn

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,31 @@ export default function Home() {
   const animate = useAnimationToggle(7000);
 
   return (
-    <div className="bg-gray-100 h-full w-full px-4 text-dark">
+    <div className=" h-full w-full px-4 text-dark">
       <div className="flex flex-col items-center text-center pt-24">
         <h1 className="text-[96px] font-semibold text-dark">
           {textData.anwendungsName}
         </h1>
-        <p className="text-dark mt-16 max-w-[800px] font-normal text-xl"  style={{ whiteSpace: "pre-wrap" }}>
+        <p
+          className="text-dark mt-16 max-w-[800px] font-normal text-xl"
+          style={{ whiteSpace: "pre-wrap" }}
+        >
           {textData.ersteInformation}
         </p>
         <button
           onClick={() => router.push("/befragung")}
           className={`${
             animate ? "animate__animated animate__headShake" : ""
-          } mt-28 px-6 py-4 bg-blue text-white font-light text-2xl rounded-lg  shadow-md flex flex-row-reverse gap-3 justify-center items-center w-[400px] transition hover:shadow-2xl hover:scale-105 ease-in`}
+          } mt-28 px-6 py-4 bg-blue text-white font-light text-2xl rounded-lg  shadow-md flex flex-row-reverse gap-3 justify-center items-center w-[400px] transition hover:shadow-2xl hover:scale-105 ease-in uppercase`}
         >
           <i className="pi pi-arrow-right" style={{ fontSize: "1rem" }} />
           {textData.startButton}
+        </button>
+        <button
+          onClick={() => router.push("/media")}
+          className="underline mt-6 text-lg text-gray-400 font-medium"
+        >
+          {textData.medienuebersichtButton}
         </button>
       </div>
 
@@ -39,7 +48,9 @@ export default function Home() {
           <h2 className="font-semibold text-3xl">
             {textData.wasIstWahlOMatHeader}
           </h2>
-          <p style={{ whiteSpace: "pre-wrap" }}>{textData.wasIstWahlOMatSubtext}</p>
+          <p style={{ whiteSpace: "pre-wrap" }}>
+            {textData.wasIstWahlOMatSubtext}
+          </p>
         </div>
 
         <div className="flex flex-col gap-6 max-w-[800px]">
