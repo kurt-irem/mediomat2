f90e288891eb549aa658656fb0dc93e2902ba6d9
cedric.hohensee <cedricc.business@gmail.com>
2024-03-29 12:16:44 +0100

    add mehr anzeigen button && add style && add text in json && create svg

--- components/cards/vorschlagCard.tsx
+++ components/cards/vorschlagCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function VorschlagCard({
   name,
   beschreibung,
@@ -5,7 +7,11 @@ export default function VorschlagCard({
   url,
   mediumArt,
   codierung,
+  cardClassName,
+  cardIndex,
 }: {
+  cardIndex?: number;
+  cardClassName?: string;
   codierung?: number;
   mediumArt?: string;
   url?: string;
@@ -13,8 +19,22 @@ export default function VorschlagCard({
   beschreibung: string;
   image: string;
 }) {
+  const [showMore, setShowMore] = useState(false);
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
   return (
-    <div className="bg-white p-6 w-[332px] min-h-[500px] rounded-2xl hover:scale-95 scale-90 flex justify-between flex-col shadow-xl border-2 border-gray-200 transition-all">
+    <div
+      className={`${cardClassName} ${
+        cardIndex === 0
+          ? "silver-border"
+          : cardIndex === 1
+          ? "gold-border"
+          : cardIndex === 2
+          ? "bronze-border"
+          : "border-2 border-gray-200"
+      } bg-white p-6 w-[332px] min-h-[700px] rounded-2xl hover:scale-95 scale-90 flex justify-between flex-col shadow-xl transition-all`}
+    >
       <div className="flex flex-col gap-6">
         <span>
           <p className="px-2 bg-gray-100 text-gray-500 w-min rounded-md font-medium tracking-[1px] uppercase text-sm">
@@ -24,7 +44,7 @@ export default function VorschlagCard({
             className="font-semibold text-2xl px-1 mt-1 uppercase"
             title={name}
           >
-            {name.length > 25 ? `${name.slice(0, 27)}...` : name}
+            {name.length > 27 ? `${name.slice(0, 27)}...` : name}
           </h2>
         </span>
         <img
@@ -32,7 +52,20 @@ export default function VorschlagCard({
           alt="medium"
           className="rounded-lg h-[200px] bg-contain"
         />
-        <p className="text-base">{beschreibung}</p>
+        {beschreibung && (
+          <p className="text-base">
+            {showMore
+              ? beschreibung
+              : `${beschreibung.slice(0, 200)}${
+                  beschreibung.length > 200 ? "..." : ""
+                }`}
+          </p>
+        )}
+        {beschreibung && beschreibung.length > 200 && (
+          <button onClick={handleClick} className="text-[#c4c4c4] rounded-md">
+            {showMore ? "Weniger anzeigen" : "mehr anzeigen..."}
+          </button>
+        )}
       </div>
       {codierung}
       <a
